bb8c5c3c902c5b94e3d463b6d0fc12e4584b83be
alivane <vaneg643@gmail.com>
2017-12-12 22:15:04 -0300

    Added filters to files and added of styles for images

--- app.js
+++ app.js
@@ -4,10 +4,25 @@
   angular.module('angularFilters', [])
   .controller('Controller', Controller)
 
-  Controller.$inject = ['$scope'];
+  Controller.$inject = ['$scope', '$filter'];
 
-  function Controller($scope) {
-    
+  function Controller($scope, $filter) {
+    var count = 0;
+    $scope.value = 3;
+    $scope.name = ' AliVane ';
+    $scope.CookieCost = .45;
+    $scope.button = function () {
+        if(count == 3) count = 0;
+        count ++;
+        $scope.value = count;
+        return count;
+    }
+    $scope.msg = function () {
+      var msg = 'Hello, How are you?';
+      var output = $filter('uppercase')(msg)
+      return output
+    }
+
   };
 
 })()
